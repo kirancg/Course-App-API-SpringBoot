2f49941685a296cc43196fd9c27f28063f1e98a0
Kiran_Kumar_Gangadharaiah <kirancg93@gmail.com>
2020-06-23 19:32:26 -0400

    application.properties

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -16,19 +16,30 @@ public class TopicController {
 	@Autowired
 	private TopicService topicService;
 	
-	@RequestMapping("/topic")
+	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 	}
 	
-	@RequestMapping("/topic/{id}")
+	@RequestMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable  String id) {
-		return topicService.getTopic(id);
-		
+		return topicService.getTopic(id);	
 	}
 	
 	@RequestMapping(method = RequestMethod.POST ,value="/topics" )
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic ,@PathVariable  String id ) {
+		 topicService.updateTopic(id,topic);	
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public void deleteTopic(@RequestBody Topic topic ,@PathVariable  String id ) {
+		 topicService.deleteTopic(id,topic);	
+	}
+	
+	
 }
